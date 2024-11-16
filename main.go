c68aef63afb8a12a3f59eeab3f69a7102abf5663
PotterSource <davidpotterstudio@gmail.com>
2024-11-16 00:57:22 -0600

    add sitemap fetch

--- main.go
+++ main.go
@@ -1,29 +1,42 @@
 package main
 
-import "github.com/Pottersource/go-sitemap-convert/models"
+import (
+	"fmt"
+	"github.com/Pottersource/go-sitemap-convert/sitemap"
+)
 
 func main() {
-	sitemapUrls := []models.SitemapUrl{
-		{
-			Location:   "https://www.example.com",
-			LastMod:    "2021-01-01",
-			ChangeFreq: "daily",
-			Priority:   "1.0",
-		},
-		{
-			Location:   "https://www.example.com/about",
-			LastMod:    "2021-01-01",
-			ChangeFreq: "daily",
-			Priority:   "0.8",
-		},
-		{
-			Location:   "https://www.example.com/contact",
-			LastMod:    "2021-01-01",
-			ChangeFreq: "daily",
-			Priority:   "0.8",
-		},
+	//sitemapUrls := []models.SitemapUrl{
+	//	{
+	//		Location:   "https://www.example.com",
+	//		LastMod:    "2021-01-01",
+	//		ChangeFreq: "daily",
+	//		Priority:   "1.0",
+	//	},
+	//	{
+	//		Location:   "https://www.example.com/about",
+	//		LastMod:    "2021-01-01",
+	//		ChangeFreq: "daily",
+	//		Priority:   "0.8",
+	//	},
+	//	{
+	//		Location:   "https://www.example.com/contact",
+	//		LastMod:    "2021-01-01",
+	//		ChangeFreq: "daily",
+	//		Priority:   "0.8",
+	//	},
+	//}
+	//
+	//sitemap := models.NewSitemap(sitemapUrls)
+	//sitemap.Output()
+
+	url := "https://developers.google.com/sitemap.xml"
+
+	data, contentType, err := sitemap.Fetch(url)
+	if err != nil {
+		panic(err)
 	}
 
-	sitemap := models.NewSitemap(sitemapUrls)
-	sitemap.Output()
+	fmt.Println("Content-Type:", contentType)
+	fmt.Println(string(data))
 }
