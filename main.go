a8b7865684f4643d2dde05f7ff1062305118888e
PotterSource <davidpotterstudio@gmail.com>
2024-11-16 00:40:32 -0600

    sitemap models

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import "github.com/Pottersource/go-sitemap-convert/models"
 
 func main() {
-	fmt.Println("Project start")
+	sitemapUrls := []models.SitemapUrl{
+		{
+			Location:   "https://www.example.com",
+			LastMod:    "2021-01-01",
+			ChangeFreq: "daily",
+			Priority:   "1.0",
+		},
+		{
+			Location:   "https://www.example.com/about",
+			LastMod:    "2021-01-01",
+			ChangeFreq: "daily",
+			Priority:   "0.8",
+		},
+		{
+			Location:   "https://www.example.com/contact",
+			LastMod:    "2021-01-01",
+			ChangeFreq: "daily",
+			Priority:   "0.8",
+		},
+	}
+
+	sitemap := models.NewSitemap(sitemapUrls)
+	sitemap.Output()
 }
